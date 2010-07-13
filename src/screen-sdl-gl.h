04c90349048ecb334f54743ac0a655f7821240bc
Alexandros Frantzis <alf82@freemail.gr>
2010-07-13 13:22:05 +0300

    Add license headers to source files.

--- src/screen-sdl-gl.h
+++ src/screen-sdl-gl.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright © 2008 Ben Smith
+ * Copyright © 2010 Linaro
+ *
+ * This file is part of the glmark2 OpenGL (ES) 2.0 benchmark.
+ *
+ * glmark2 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3, as published by the Free
+ * Software Foundation.
+ *
+ * glmark2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * glmark2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *  Ben Smith (original glmark benchmark)
+ *  Alexandros Frantzis (glmark2)
+ */
 #ifndef _SCREEN_SDL_GL_H
 #define _SCREEN_SDL_GL_H
 
