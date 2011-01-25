77036b535efe16619b8795528ffd5834928d339d
Alexandros Frantzis <alexandros.frantzis@linaro.org>
2011-01-25 17:00:12 +0200

    Update copyright for 2011. Update license to GPL-3+ (instead of just GPL-3).

--- src/matrix.h
+++ src/matrix.h
@@ -1,11 +1,12 @@
 /*
- * Copyright © 2010 Linaro
+ * Copyright © 2010-2011 Linaro Limited
  *
  * This file is part of the glmark2 OpenGL (ES) 2.0 benchmark.
  *
  * glmark2 is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License version 3, as published by the Free
- * Software Foundation.
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
  * glmark2 is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
--- src/model.h
+++ src/model.h
@@ -1,12 +1,13 @@
 /*
  * Copyright © 2008 Ben Smith
- * Copyright © 2010 Linaro
+ * Copyright © 2010-2011 Linaro Limited
  *
  * This file is part of the glmark2 OpenGL (ES) 2.0 benchmark.
  *
  * glmark2 is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License version 3, as published by the Free
- * Software Foundation.
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
  * glmark2 is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
