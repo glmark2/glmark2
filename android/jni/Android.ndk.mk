bfbe3659983d0965e1560ce382f9d6768c5e9ff6
Alexandros Frantzis <alexandros.frantzis@linaro.org>
2011-08-16 14:53:03 +0300

    Improve module names.
    Use module name with a better chance of being unique. Also prefix all module
    names with 'lib', to reduce the diff from the conventions used by the Android
    build system (this is fine for NDK builds, too).

--- android/jni/Android.ndk.mk
+++ android/jni/Android.ndk.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/src/libmatrix
-LOCAL_MODULE := matrix
+LOCAL_MODULE := libglmark2-matrix
 LOCAL_CFLAGS := -DUSE_GLESv2
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_SRC_FILES := src/libmatrix/mat.cc \
@@ -14,7 +14,7 @@ include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := png
+LOCAL_MODULE := libglmark2-png
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/src/libpng
 LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/libpng/*.c))
 
@@ -23,8 +23,8 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := glmark2-android
-LOCAL_STATIC_LIBRARIES := matrix png
+LOCAL_MODULE := libglmark2-android
+LOCAL_STATIC_LIBRARIES := libglmark2-matrix libglmark2-png
 LOCAL_CFLAGS := -DUSE_GLESv2 -DGLMARK_DATA_PATH=""
 LOCAL_LDLIBS := -landroid -llog -lGLESv2 -lEGL -lz -lm
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
