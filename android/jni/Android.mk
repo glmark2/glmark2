7cdb36b38038055d67c569428ed8ee39cadb9f46
Alexandros Frantzis <alexandros.frantzis@linaro.org>
2011-08-16 11:28:39 +0300

    Don't explicitly set ANDROID preprocessor define during build.
    The ANDROID preprocessor define is automatically set by the Android build
    system/NDK.

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/src/libmatrix
 LOCAL_MODULE := matrix
-LOCAL_CPPFLAGS := -DANDROID -DUSE_GLESv2
+LOCAL_CPPFLAGS := -DUSE_GLESv2
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_SRC_FILES := src/libmatrix/mat.cc \
                    src/libmatrix/program.cc
@@ -25,7 +25,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := glmark2-android
 LOCAL_STATIC_LIBRARIES := matrix png
-LOCAL_CPPFLAGS := -DANDROID -DUSE_GLESv2 -DGLMARK_DATA_PATH=""
+LOCAL_CPPFLAGS := -DUSE_GLESv2 -DGLMARK_DATA_PATH=""
 LOCAL_LDLIBS := -landroid -llog -lGLESv2 -lEGL -lz -lm
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_SRC_FILES := $(filter-out src/canvas% src/main.cpp, \
