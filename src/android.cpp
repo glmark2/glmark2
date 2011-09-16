b4b38e5702ba235028ac11c9b6b11b159d728549
Alexandros Frantzis <alexandros.frantzis@linaro.org>
2011-09-16 11:29:57 +0300

    Android: Fix typo in desktop scene registration statement.

--- src/android.cpp
+++ src/android.cpp
@@ -86,7 +86,7 @@ Java_org_linaro_glmark2_Glmark2Renderer_nativeInit(JNIEnv* env, jclass clazz,
     Benchmark::register_scene(*new SceneBump(*g_canvas));
     Benchmark::register_scene(*new SceneEffect2D(*g_canvas));
     Benchmark::register_scene(*new ScenePulsar(*g_canvas));
-    Benchmark::register_scene(*new SceneDesktop(canvas));
+    Benchmark::register_scene(*new SceneDesktop(*g_canvas));
 
     add_default_benchmarks(g_benchmarks);
 
