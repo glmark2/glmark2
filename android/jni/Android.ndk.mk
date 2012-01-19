7f3f09cc59db85e2b9453a43d1428e7c62b513ba
Alexandros Frantzis <alexandros.frantzis@linaro.org>
2012-01-19 20:13:38 +0200

    Build,Doc: Update files for 2012.01 release.

--- android/jni/Android.ndk.mk
+++ android/jni/Android.ndk.mk
@@ -23,7 +23,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libglmark2-android
 LOCAL_STATIC_LIBRARIES := libglmark2-matrix libglmark2-png
-LOCAL_CFLAGS := -DUSE_GLESv2 -DGLMARK_DATA_PATH="" -DGLMARK_VERSION="\"2011.12\""
+LOCAL_CFLAGS := -DUSE_GLESv2 -DGLMARK_DATA_PATH="" -DGLMARK_VERSION="\"2012.01\""
 LOCAL_LDLIBS := -landroid -llog -lGLESv2 -lEGL -lz
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src \
                     $(LOCAL_PATH)/src/libmatrix \
