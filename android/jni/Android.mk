d1d296ea4d2607f4c395bbbb1e031f27d54b7fb0
Alexandros Frantzis <alexandros.frantzis@linaro.org>
2012-04-03 11:38:00 +0300

    Android: Don't treat unused parameters as an error.
    The AOSP ships some header files which contain functions with unused parameters.
    Because we have strict compilation flags, the unused parameters break the build.
    To work around this issue, this commit stops treating unused parameters as an
    error, at least until the AOSP code is fixed upstream.

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -4,7 +4,8 @@ include $(CLEAR_VARS)
 
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_MODULE := libglmark2-matrix
-LOCAL_CFLAGS := -DUSE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor
+LOCAL_CFLAGS := -DUSE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor \
+                -Wno-error=unused-parameter
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/libmatrix/*.cc))
 LOCAL_SHARED_LIBRARIES := libdl libstlport
@@ -27,7 +28,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libglmark2-android
 LOCAL_STATIC_LIBRARIES := libglmark2-matrix libglmark2-png
 LOCAL_CFLAGS := -DGLMARK_DATA_PATH="" -DGLMARK_VERSION="\"2012.03\"" \
-                -DUSE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor
+                -DUSE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor \
+                -Wno-error=unused-parameter
 LOCAL_SHARED_LIBRARIES := liblog libz libEGL libGLESv2 libandroid libdl libstlport
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src \
                     $(LOCAL_PATH)/src/libmatrix \
