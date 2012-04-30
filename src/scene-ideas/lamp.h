b3cff37bb23c31790a02c368a600c7c6bac203ba
Jesse Barker <jesse.barker@linaro.org>
2012-04-30 11:37:22 -0700

    SceneIdeas: Add copyright notices to all source files, and add the general notice to the "COPYING" data.  Also, remove one extraneous file that snuck into a previous add undetected.

--- src/scene-ideas/lamp.h
+++ src/scene-ideas/lamp.h
@@ -1,3 +1,25 @@
+/*
+ * (c) Copyright 1993, Silicon Graphics, Inc.
+ * Copyright © 2012 Linaro Limited
+ *
+ * This file is part of the glmark2 OpenGL (ES) 2.0 benchmark.
+ *
+ * glmark2 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * glmark2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * glmark2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *  Jesse Barker
+ */
 #ifndef LAMP_H_
 #define LAMP_H_
 
