4893627d4e7f7a1a9ef2086b00522c7c546b1ea1
Alexandros Frantzis <alexandros.frantzis@linaro.org>
2012-05-24 12:25:58 +0300

    Android: Don't treat unused parameter warnings as errors when building with the NDK.
    The stlport C++ library exports code with unused parameters, causing builds
    to fail if we treat unused parameter warnings as errors.

--- android/jni/Android.ndk.mk
+++ android/jni/Android.ndk.mk
@@ -4,7 +4,8 @@ include $(CLEAR_VARS)
 
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_MODULE := libglmark2-matrix
-LOCAL_CFLAGS := -DUSE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor
+LOCAL_CFLAGS := -DUSE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor \
+                -Wno-error=unused-parameter
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/libmatrix/*.cc))
 
@@ -22,7 +23,7 @@ include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_MODULE := libglmark2-ideas
 LOCAL_CFLAGS := -DGLMARK_DATA_PATH="" -DUSE_GLESv2 -Werror -Wall -Wextra\
-                -Wnon-virtual-dtor
+                -Wnon-virtual-dtor -Wno-error=unused-parameter
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src \
                     $(LOCAL_PATH)/src/libmatrix
 LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/scene-ideas/*.cc))
@@ -35,7 +36,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libglmark2-android
 LOCAL_STATIC_LIBRARIES := libglmark2-matrix libglmark2-png libglmark2-ideas
 LOCAL_CFLAGS := -DGLMARK_DATA_PATH="" -DGLMARK_VERSION="\"2012.03\"" \
-                -DUSE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor
+                -DUSE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor \
+                -Wno-error=unused-parameter
 LOCAL_LDLIBS := -landroid -llog -lGLESv2 -lEGL -lz
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src \
                     $(LOCAL_PATH)/src/libmatrix \
