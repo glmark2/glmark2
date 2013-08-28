963c1a0ce73b7a0f3bd8a082abe4b33eefe9a300
Rafał Mielniczuk <rafal.mielniczuk2@gmail.com>
2013-08-28 19:43:48 +0200

    NativeStateWayland: use wl_display_roundtrip in flip method
    As noticed by Axel Davy we need to use wl_display_roundtrip in
    NativeStateWayland::flip to prevent blocking when eglSwapInterval(0) is
    set.
    
    More info:
    https://bugs.launchpad.net/glmark2/+bug/1211076

--- src/native-state-wayland.cpp
+++ src/native-state-wayland.cpp
@@ -279,7 +279,7 @@ NativeStateWayland::should_quit()
 void
 NativeStateWayland::flip()
 {
-    int ret = wl_display_dispatch(display_->display);
+    int ret = wl_display_roundtrip(display_->display);
     should_quit_ = (ret == -1) || should_quit_;
 }
 
