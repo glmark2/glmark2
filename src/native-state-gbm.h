de8df1efd09745999270a375aaba306f37f7cff7
Erico Nunes <nunes.erico@gmail.com>
2022-04-23 09:37:11 +0200

    NativeStateGBM: Add GBM offscreen backend
    glmark2 supports offscreen runs and even GBM already in the case of the
    DRM backend. However, in all cases even with the --off-screen flag, it
    goes through display or a window system initialization which requires
    connected hardware or additional environment setup to satisfy that
    initialization (connected monitor or Xorg or a Wayland compositor
    running).
    
    This commit adds a simple GBM backend which is purely offscreen and
    requires no display or window system setup. The advantage is being able
    to run on systems with no display-related requirements, and providing
    GPU benchmark results completely independent of displays. It is of
    course not possible to visually see the results, as is already the case
    with other offscreen runs.
    
    The benchmark results with are equivalent, for the same surface size, to
    a run with other backends that work in a similar fashion, so compatible
    with current glmark2 scores. It is also possible to run with the
    --off-screen flag for the same alternate offscreen framebuffer path as
    other backends.
    
    This backend can be helpful in, for instance: development systems,
    automated test runs, CI environments, or runs on headless remote
    systems.
    
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>

---
+++ src/native-state-gbm.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2022 Erico Nunes
+ *
+ * This file is part of the glmark2 OpenGL (ES) 2.0 benchmark.
+ *
+ * glmark2 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * glmark2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * glmark2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GLMARK2_NATIVE_STATE_GBM_H_
+#define GLMARK2_NATIVE_STATE_GBM_H_
+
+#include "native-state.h"
+#include <csignal>
+#include <cstring>
+#include <gbm.h>
+
+class NativeStateGBM : public NativeState
+{
+public:
+    NativeStateGBM() :
+        fd_(0),
+        dev_(0),
+        surface_(0) {}
+    ~NativeStateGBM() { cleanup(); }
+
+    bool init_display();
+    void* display();
+    bool create_window(WindowProperties const& properties);
+    void* window(WindowProperties& properties);
+    void visible(bool v);
+    bool should_quit();
+    void flip();
+
+private:
+    static void quit_handler(int signum);
+    static volatile std::sig_atomic_t should_quit_;
+
+    void cleanup();
+
+    int width;
+    int height;
+
+    int fd_;
+    gbm_device* dev_;
+    gbm_surface* surface_;
+};
+
+#endif /* GLMARK2_NATIVE_STATE_GBM_H_ */
